90b25e0587bab4e7d462d5ff5c84eaa66aee746b
kishorazad <brizkishor.azad@gmail.com>
2026-04-26 01:48:40 +0530

    Update products.ts

--- api/products.ts
+++ api/products.ts
@@ -1,33 +1,20 @@
-
 import { Pool } from "@neondatabase/serverless";
 
-export default async function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default async function handler(req: any, res: any) {
   try {
-    // LINE 10 → CHECK ENV VARIABLE
-    if (!process.env.DATABASE_URL) {
-      throw new Error("DATABASE_URL missing");
-    }
-
-    // LINE 15 → CREATE CONNECTION
     const pool = new Pool({
       connectionString: process.env.DATABASE_URL,
     });
 
-    // LINE 20 → FETCH PRODUCTS
     const result = await pool.query("SELECT * FROM products");
 
-    // LINE 23 → RESPONSE
     res.status(200).json({
       success: true,
       data: result.rows,
     });
 
   } catch (err: any) {
-    // LINE 30 → ERROR HANDLING
-    console.error("ERROR:", err);
+    console.error(err);
 
     res.status(500).json({
       success: false,
