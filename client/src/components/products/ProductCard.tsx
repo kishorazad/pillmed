ef25a3ccffe486a25737b0622acf18181a3cfc5f
kishorazad <brizkishor.azad@gmail.com>
2026-04-26 14:35:48 +0530

    Update ProductCard.tsx

--- client/src/components/products/ProductCard.tsx
+++ client/src/components/products/ProductCard.tsx
@@ -98,11 +98,13 @@ const ProductCard = ({ product }: ProductCardProps) => {
         <div className="flex items-center justify-between">
           <div>
             <span className="font-bold">
-              ₹{product.discountedPrice || product.price}
+              {/* ₹{product.discountedPrice || product.price} */}
+              ₹{Number(product.discountedPrice || product.price || 0).toFixed(2)}
             </span>
             {product.discountedPrice && (
               <span className="text-xs text-gray-500 line-through ml-1">
-                ₹{product.price}
+                {/* ₹{product.price} */}
+                ₹{Number(product.price || 0).toFixed(2)}
               </span>
             )}
           </div>
