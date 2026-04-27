2f1a94f72d7eb548a0da4c05a87779866b8da2e9
kishorazad <brizkishor.azad@gmail.com>
2026-04-27 21:04:29 +0530

    Simplify handler function by removing types

--- api/cart/[id].ts
+++ api/cart/[id].ts
@@ -1,9 +1,4 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(
-  req: VercelRequest,
-  res: VercelResponse
-) {
+export default function handler(req, res) {
   res.status(200).json({
     items: [],
     total: 0
